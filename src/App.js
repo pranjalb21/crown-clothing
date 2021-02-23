a8e64ff117808dc139600350b301608a757ea3f4
Pranjal <pranjalgain052@gmail.com>
2021-02-23 21:24:37 +0530

    Basic structure is made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+
+import Homepage from './hompage.component';
+
 import './App.css';
 
 function App() {
-  return  <div>Hello World</div>;
+  return  (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
