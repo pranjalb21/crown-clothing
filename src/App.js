a4c39b79103de3bd5a8f2151d3fc418158e34cbf
Pranjal <pranjalgain052@gmail.com>
2021-04-04 10:33:36 +0530

    Header added and linked shop page and contact page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
 
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats</h1>
-  </div>
-);
 
 function App() {
   return  (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={ Homepage }/>
         <Route exact path='/shop' component={ ShopPage }/>
-        <Route exact path='/hats' component={ HatsPage }/>
         
       </Switch>
     </div>
